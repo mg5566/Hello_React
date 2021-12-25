00f9a1813a9174999abdd6fee089196910744396
mg5566 <mg5566@hanmail.net>
2021-12-25 22:39:23 +0900

    feat: search form (#3)
    * feat: create search form
    
    * refactor: refactoring element to component
    
    * feat: add state
    
    * feat: add input element chage event
    
    * refactor: refactoring change handler using setState
    
    * feat: add reset buttonfeature
    
    * feat: add other ways
    
    * feat: add submit event
    
    * feat: add reset event and event handler

--- js/main.js
+++ js/main.js
@@ -1,7 +1,70 @@
-const element = (
-  <header>
-    <h2 className="container">검색</h2>
-  </header>
-);
+// TODO
+class App extends React.Component {
+  // TODO
+  constructor() {
+    super();
 
-ReactDOM.render(element, document.querySelector("#app"));
+    this.state = {
+      searchKeyword: "",
+    };
+  }
+
+  // TODO
+  handleChangeInput(event) {
+    const searchKeyword = event.target.value;
+
+    this.setState({
+      searchKeyword,
+    });
+  }
+
+  // TODO
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('handleSubmit', event, this.state.searchKeyword);
+  }
+
+  // TODO
+  handleReset() {
+    this.setState({
+      searchKeyword: "",
+    });
+    this.setState(() => {
+      return { searchKeyword: "" }
+    }, () => {
+      console.log("handleReset", this.state.searchKeyword);
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <header>
+          <h2 className="container">검색</h2>
+        </header>
+        {/* TODO */}
+        <div className="container">
+          <form
+            onSubmit={(event) => this.handleSubmit(event)}
+            // onReset={() => this.handleReset()}
+          >
+            <input
+              type="text"
+              placeholder="검색어를 입력하세요"
+              autoFocus
+              value={this.state.searchKeyword}
+              // TODO
+              onChange={(event) => this.handleChangeInput(event)}
+            />
+            {/* TODO */}
+            {this.state.searchKeyword.length > 0 && (
+              <button type="reset" className="btn-reset" onClick={() => this.handleReset()}></button>
+            )}
+          </form>
+        </div>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"));
