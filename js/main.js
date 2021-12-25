717575a693badd04e8e968462ade6e84fb111a34
강명구 <mg5566@hanmail.net>
2021-12-25 20:44:19 +0900

    refactor: refactoring element to component

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
-const element = (
-  <>
-    <header>
-      <h2 className="container">검색</h2>
-    </header>
-    {/* TODO */}
-    <div className="container">
-      <form>
-        <input type="text" placeholder="검색어를 입력하세요" autoFocus />
-        <button type="reset" className="btn-reset"></button>
-      </form>
-    </div>
-  </>
-);
+// TODO
+class App extends React.Component {
+  render() {
+    return (
+        <>
+        <header>
+          <h2 className="container">검색</h2>
+        </header>
+        {/* TODO */}
+        <div className="container">
+          <form>
+            <input type="text" placeholder="검색어를 입력하세요" autoFocus />
+            <button type="reset" className="btn-reset"></button>
+          </form>
+        </div>
+      </>
+    );
+  }
+}
 
-ReactDOM.render(element, document.querySelector("#app"));
+ReactDOM.render(<App />, document.querySelector("#app"));
