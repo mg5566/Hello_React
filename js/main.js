c5f4bfcb55c288b3482ef9af9d7314d6f30e31a2
mg5566 <mg5566@hanmail.net>
2021-12-25 23:57:11 +0900

    feat: search result (#4)
    * feat: add searchResult state
    
    * feat: add search result frame
    
    * refactor: print search result when is searchResult array
    
    * feat: add Model
    
    * refactor: change to singleton pattern
    
    * feat: add helpers.js
    
    * feat: print search result
    
    * feat: add submitted state
    
    * refactor: refactor element
    
    * feat: make init search result

--- js/main.js
+++ js/main.js
@@ -1,66 +1,104 @@
-// TODO
+import store from "./js/Store.js";  // 이게 중요하네...
+
 class App extends React.Component {
-  // TODO
   constructor() {
     super();
 
     this.state = {
       searchKeyword: "",
+      searchResult: [],
+      submited: false,
     };
+
+    this.store = store;
   }
 
-  // TODO
   handleChangeInput(event) {
     const searchKeyword = event.target.value;
+
+    if (searchKeyword.length <= 0 && this.state.submited) {
+      return this.handleReset();
+    }
 
     this.setState({
       searchKeyword,
     });
   }
 
-  // TODO
   handleSubmit(event) {
     event.preventDefault();
-    console.log('handleSubmit', event, this.state.searchKeyword);
+    if (this.state.searchKeyword.length <= 0) {
+      return;
+    }
+    this.search(this.state.searchKeyword);
   }
 
-  // TODO
+  search(searchKeyword) {
+    const searchResult = store.search(searchKeyword);
+    this.setState({
+      searchResult,
+      submited: true,
+    });
+  }
+
   handleReset() {
     this.setState({
       searchKeyword: "",
+      // searchResult: [],
+      submited: false,
     });
     this.setState(() => {
       return { searchKeyword: "" }
     }, () => {
-      console.log("handleReset", this.state.searchKeyword);
     });
   }
 
   render() {
+    const searchForm = (
+      <form
+        onSubmit={(event) => this.handleSubmit(event)}
+        onReset={() => this.handleReset()}
+      >
+        <input
+          type="text"
+          placeholder="검색어를 입력하세요"
+          autoFocus
+          value={this.state.searchKeyword}
+          onChange={(event) => this.handleChangeInput(event)}
+        />
+        {this.state.searchKeyword.length > 0 && (
+          <button type="reset" className="btn-reset"></button>
+        )}
+      </form>
+    );
+
+    const searchResult = (
+      this.state.searchResult.length > 0 ? (
+        <ul className="result">
+          {this.state.searchResult.map((item) => {
+            return (
+              <li key={item.id}>
+                <img src={item.imageUrl} alt={item.name}/>
+                <p>{item.name}</p>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <div className="empty-box">검색결과는 없습니다.</div>
+      )
+    );
+
     return (
       <>
         <header>
           <h2 className="container">검색</h2>
         </header>
-        {/* TODO */}
         <div className="container">
-          <form
-            onSubmit={(event) => this.handleSubmit(event)}
-            // onReset={() => this.handleReset()}
-          >
-            <input
-              type="text"
-              placeholder="검색어를 입력하세요"
-              autoFocus
-              value={this.state.searchKeyword}
-              // TODO
-              onChange={(event) => this.handleChangeInput(event)}
-            />
-            {/* TODO */}
-            {this.state.searchKeyword.length > 0 && (
-              <button type="reset" className="btn-reset" onClick={() => this.handleReset()}></button>
-            )}
-          </form>
+          {searchForm}
+          <div className="content">
+            {this.state.submited && searchResult}
+          </div>
         </div>
       </>
     );
