bfaf18cdf5ca430eedba8ea36b0e0ebaff3ddb1d
mg5566 <mg5566@hanmail.net>
2021-12-27 22:50:04 +0900

    feat: header component (#9)
    * feat: header component
    
    * feat: apply Header component to App component

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -1,9 +1,13 @@
+
 import React from "react";
+import Header from "./Header.js";
 
 export default class App extends React.Component {
   render() {
     return (
-      <>TODO</>
+      <>
+        <Header />
+      </>
     );
   }
 }
