a8bd6cf17cc8b44efa4bf50ad4a069ec6aece7df
강명구 <mg5566@hanmail.net>
2021-12-27 14:49:46 +0900

    feat: add get history list function

--- js/Store.js
+++ js/Store.js
@@ -17,9 +17,9 @@ class Store {
     return this.storage.keywordData;
   }
 
-  // getHistoryList() {
-  //   return this.storage.historyData.sort(this._sortHistory);
-  // }
+  getHistoryList() {
+    return this.storage.historyData.sort(this._sortHistory);
+  }
 
   // _sortHistory(history1, history2) {
   //   return history2.date > history1.date;
