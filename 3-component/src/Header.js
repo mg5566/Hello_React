5d5c8c03696ea78a3cf573e651a3dc3bfc380780
강명구 <mg5566@hanmail.net>
2021-12-27 23:08:02 +0900

    feat: add another way

--- 3-component/src/Header.js
+++ 3-component/src/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 // TODO
+// const Header = (props) => {
 const Header = ({ title }) => {
   return (
     <header>
+      {/* <h2 className="container">{props.title}</h2> */}
       <h2 className="container">{title}</h2>
     </header>
   );
