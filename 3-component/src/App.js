ea7f9712156b97e0c33bdd1fd13948ebc968f258
강명구 <mg5566@hanmail.net>
2021-12-27 23:46:27 +0900

    feat: add reset and submit event

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -3,12 +3,25 @@ import Header from "./components/Header.js";
 import SearchForm from "./components/SearchForm.js";
 
 export default class App extends React.Component {
+  // TODO
+  search(searchKeyword) {
+    console.log("App search()", searchKeyword);
+  }
+
+  handleReset() {
+    console.log("TODO: reset button");
+  }
+
   render() {
     return (
       <>
         <Header title={"Searching"} />
         <div className="container">
-          <SearchForm />
+          {/* TODO */}
+          <SearchForm
+            onSubmit={(searchKeyword) => this.search(searchKeyword)}
+            onReset={() => this.handleReset()}
+          />
         </div>
       </>
     );
--- 3-component/src/components/SearchForm.js
+++ 3-component/src/components/SearchForm.js
@@ -11,14 +11,28 @@ export default class SearchForm extends React.Component {
 
   handleChangeInput(event) {
     const searchKeyword = event.target.value;
+
+    if (searchKeyword.length <= 0) {
+      this.handleReset();
+    }
+
     this.setState({ searchKeyword });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.state.searchKeyword);
+  }
+
+  handleReset() {
+    this.props.onReset();
   }
 
   render() {
     return (
       <form
-        // onSubmit={(event) => this.handleSubmit(event)}
-        // onReset={() => this.handleReset()}
+        onSubmit={(event) => this.handleSubmit(event)}
+        onReset={() => this.handleReset()}
       >
         <input
           type="text"
