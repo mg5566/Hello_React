f7dbbbeaf141e1b950ae67e77cf075714ff7f3f7
강명구 <mg5566@hanmail.net>
2021-12-28 21:38:40 +0900

    feat: add Store Model

---
+++ 3-component/src/Store.js
@@ -0,0 +1,61 @@
+import { createNextId } from "./helpers.js";
+import storage from "./storage.js";
+
+class Store {
+  constructor(storage) {
+    if (!storage) throw "no storage";
+
+    this.storage = storage;
+  }
+
+  search(keyword) {
+    this.addHistory(keyword);
+
+    return (this.storage.productData.filter((product) =>
+      product.name.includes(keyword)
+    ));
+  }
+
+  getKeywordList() {
+    return this.storage.keywordData;
+  }
+
+  getHistoryList() {
+    return this.storage.historyData.sort(this._sortHistory);
+  }
+
+  _sortHistory(history1, history2) {
+    return history2.date > history1.date;
+  }
+
+  removeHistory(keyword) {
+    this.storage.historyData = this.storage.historyData.filter(
+      (history) => history.keyword !== keyword
+    );
+  }
+
+  // TODO
+  addHistory(keyword = "") {
+    keyword = keyword.trim();
+    if (!keyword) {
+      return;
+    }
+
+    // 이미 존재하는 지 확인하는 함수 구현
+    const hasHistory = this.storage.historyData.some(
+      (history) => history.keyword === keyword
+    );
+    // 이미 존재하면 지우고, 새로 등록해서 date 를 최신화시킨다.
+    if (hasHistory) this.removeHistory(keyword);
+
+    // 새로 추가하기
+    const id = createNextId(this.storage.historyData);
+    const date = new Date();
+    this.storage.historyData.push({ id, keyword, date });
+    this.storage.historyData = this.storage.historyData.sort(this._sortHistory);
+  }
+}
+
+const store = new Store(storage);
+
+export default store;
