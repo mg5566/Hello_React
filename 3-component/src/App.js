6b2f56fee5ae32b0fe74caa641dad2cf651801f2
mg5566 <mg5566@hanmail.net>
2021-12-28 21:16:58 +0900

    feat: search form component  (#11)
    * feat: create SearchForm class component
    
    * feat: add SearchForm component to App component
    
    * feat: add reset and submit event
    
    * refactor: move position of state to base Component
    
    * refactor: remove commnet

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -1,13 +1,42 @@
-
 import React from "react";
-import Header from "./Header.js";
+import Header from "./components/Header.js";
+import SearchForm from "./components/SearchForm.js";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchKeyword: "",
+    }
+  }
+  search(searchKeyword) {
+    console.log("App search()", searchKeyword);
+  }
+
+  handleReset() {
+    console.log("TODO: reset button");
+  }
+
+  handleChangeInput(searchKeyword) {
+    if (searchKeyword.length <= 0) {
+      this.handleReset();
+    }
+    this.setState({ searchKeyword });
+  }
+
   render() {
     return (
       <>
-        {/* TODO */}
         <Header title={"Searching"} />
+        <div className="container">
+          <SearchForm
+            value={this.state.searchKeyword}
+            onChange={(value) => this.handleChangeInput(value)}
+            onSubmit={() => this.search(this.state.searchKeyword)}
+            onReset={() => this.handleReset()}
+          />
+        </div>
       </>
     );
   }
