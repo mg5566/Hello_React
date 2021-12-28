86da3d716bd6c31ab880fe0a7cfab37de8807bb7
강명구 <mg5566@hanmail.net>
2021-12-28 23:34:49 +0900

    feat: create HistoryList component

---
+++ 3-component/src/components/HistoryList.js
@@ -0,0 +1,35 @@
+import React from "react";
+import List from "./List.js";
+import store from "../Store.js";
+import { formatRelativeDate } from "../helpers";
+
+export default class HistoryList extends List {
+  componentDidMount() {
+    const data = store.getHistoryList();
+    this.setState({ data });
+  }
+
+  fetch() {
+    const data = store.getHistoryList();
+    this.setState({ data });
+  }
+
+  handleClickRemoveHistory(event, keyword) {
+    event.stopPropagation();
+    store.removeHistory(keyword);
+    this.fetch();
+  }
+
+  renderItem(item) {
+    return (
+      <>
+        <span>{item.keyword}</span>
+        <span className="date">{formatRelativeDate(item.date)}</span>
+        <button
+          className="btn-remove"
+          onClick={(event) => this.handleClickRemoveHistory(event, item.keyword)}
+        ></button>
+      </>
+    );
+  }
+}
